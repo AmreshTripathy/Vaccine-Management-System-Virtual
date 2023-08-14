71e141a76813ab3f1257e9e35fb80d433332a397
AmreshTripathy <amreshtripathy184@gmail.com>
2023-08-14 22:40:14 +0530

    Push 5

--- src/main/java/com/example/VaccineManagementSystem/VaccineManagementSystemApplication.java
+++ src/main/java/com/example/VaccineManagementSystem/VaccineManagementSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
 public class VaccineManagementSystemApplication {
 
 	public static void main(String[] args) {
